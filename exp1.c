6ed1364f75468d050a994a55fdf55c68e70fba59
Your Name <you@example.com>
2024-10-23 10:56:47 +0530

    fdgg

---
+++ exp1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+int i,n,small,large;
+printf("enter the size of an array");
+scanf("%d",&n);
+int a[n];
+printf("enter the elements:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+small=a[0];
+large=a[0];
+for(i=1;i<n;i++){
+if(a[i]<small){
+small=a[i];}
+if(a[i]>large){
+large=a[i];}
+}
+printf("smallest number:%d",small);
+printf("largest number:%d",large);
+return 0;}
