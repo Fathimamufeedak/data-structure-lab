24dbda1f0fb16a2110e56520a5624e9d4d87b185
Fathimamufeedak <24mcag09@meaec.edu.in>
2024-11-04 16:08:03 +0530

    Delete python2.py

--- python2.py
+++
@@ -1,6 +0,0 @@
-num1=float(input("enter the first number:"))
-num2=float(input("enter the second number:"))
-num3=float(input("enter the third number"))
-largest=max(num1,num2,num3)
-print("the largest number is:",largest)
-        
