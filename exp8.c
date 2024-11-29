5cb693b0969b6ac2b98cb04b17da07fec9c01501
Fathimamufeedak <24mcag09@meaec.edu.in>
2024-11-29 14:36:40 +0530

    Update exp8.c

--- exp8.c
+++ exp8.c
@@ -1,217 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int n, count = 0;
-
-void insert_begin();
-void insert_end();
-void insert_pos();
-void traversal();
-void delt_begin();
-void delt_end();
-void delt_pos();
-void search();
-
-struct node {
-    int data;
-    struct node *rlink;
-    struct node *llink;
+struct node{
+        int data;
+        struct node *next;
 };
 
-struct node *head = NULL, *newnode, *temp, *prev;
+typedef struct node node;
+node *head;
+                    default:
+                        printf("\nInvalid Operation! Try Again... \n");
+                }
+        }while(1);
+        return 0;}   
+
+void push(){
+int data;
+        node *temp,*pre;
+
+        printf("\nEnter value : ");
+        scanf("%d",&data);
+
+        temp=(node*)malloc(sizeof(node));
+        temp->data=data;
+
+        if(head == NULL){
+                head=temp;
+                head->next=NULL;
+        }
+        else{
+                pre=head;
+                while(pre->next != NULL){
+                        pre=pre->next;
+                }
+                pre->next=temp;
+                temp->next=NULL;
+        }
+}
+void pop(){
+        node *temp;
+        temp=head;
+        if(head == NULL){
+                printf("\nStack is Empty \n");
+        }
+        else{
+                while(temp->next->next != NULL){
+                temp=temp->next;
+        }
+        temp->next=NULL;
+        }
+}
+
+void peek(){
+        node *temp;
+        int count=1;
+        temp=head;
+        if(head == NULL){
+                printf("\nStack is Empty \n");
+        }
+        else{
+        while(temp->next != NULL){
+                temp=temp->next;
+                count++;
+        }
+        printf("Stack [%d] : %d\n",count,temp->data);
+}
+}
+void display(){
+        node *temp;
+        int count=1;
+        temp=head;
+        if(head == NULL){
+                printf("\nList is Empty \n");
+        }
+        else{
+       while(temp->next != NULL){
+                printf("List [%d] : %d\n",count,temp->data);
+                temp=temp->next;
+                count++;
+        }
+        printf("List [%d] : %d\n",count,temp->data);
+}
+}
 
 int main() {
-    int ch;
-    while (1) {
-        printf("\nEnter your choice:\n");
-        printf("1. Insert at Beginning\n");
-        printf("2. Insert at End\n");
-        printf("3. Insert at a Position\n");
-        printf("4. Traversal\n");
-        printf("5. Deletion at Beginning\n");
-        printf("6. Deletion at End\n");
-        printf("7. Deletion at a Position\n");
-        printf("8. Search\n");
-        printf("9. Exit\n");
-       scanf("%d", &ch);
-
-        switch (ch) {
-            case 1: insert_begin(); break;
-            case 2: insert_end(); break;
-            case 3: insert_pos(); break;
-            case 4: traversal(); break;
-            case 5: delt_begin(); break;
-            case 6: delt_end(); break;
-            case 7: delt_pos(); break;
-            case 8: search(); break;
-            case 9: exit(0);
-            default: printf("Invalid choice\n");
-        }
-    }
-    return 0;
-}
-
-void insert_begin() {
-    newnode = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the data: ");
-    scanf("%d", &newnode->data);
-    newnode->rlink = head;
-    newnode->llink = NULL;
-    if (head != NULL) {
-        head->llink = newnode;
-    }
-    head = newnode;
-    count++;
-}
-
-void traversal() {
-    if (head == NULL) {
-        printf("No nodes present\n");
-    } else {
-        printf("The elements are: ");
-        temp = head;
-        while (temp != NULL) {
-            printf("%d -> ", temp->data);
-            temp = temp->rlink;
-        }
-        printf("NULL\n");
-    }
-}
-
-void insert_end() {
-    if (head == NULL) {
-        insert_begin();
-    } else {
-        newnode = (struct node *)malloc(sizeof(struct node));
-        printf("Enter the data: ");
-        scanf("%d", &newnode->data);
-        newnode->rlink = NULL;
-        temp = head;
-        while (temp->rlink != NULL) {
-            temp = temp->rlink;
-        }
-        temp->rlink = newnode;
-        newnode->llink = temp;
-        count++;
-    }
-}
-
-void insert_pos() {
-   if (head == NULL) {
-        printf("since there is no elements left we add into first position");
-        insert_begin();
-    } else {
-        printf("Enter the position to insert: ");
-        scanf("%d", &n);
-        if (n > count + 1) {
-            printf("Insertion not possible\n");
-        } else if(n == 1) {
-            insert_begin();
-        } else {
-            newnode = (struct node *)malloc(sizeof(struct node));
-            printf("Enter the data: ");
-            scanf("%d", &newnode->data);
-            int i = 1;
-            temp = head;
-            while (i < n - 1 && temp->rlink != NULL) {
-                temp = temp->rlink;
-                i++;
-            }
-            newnode->rlink = temp->rlink;
-            if (temp->rlink != NULL) {
-                temp->rlink->llink = newnode;
-            }
-            temp->rlink = newnode;
-            newnode->llink = temp;
-            count++;
-        }
-    }
-}
-
-void delt_begin() {
-    if (head == NULL) {
-        printf("No nodes found\n");
-    } else {
-        temp = head;
-        head = head->rlink;
-        if (head != NULL) {
-            head->llink = NULL;
-        }
-        printf("Deleted element is %d\n", temp->data);
-        free(temp);
-        count--;
-    }
-}
-
-void delt_end() {
-    if (head == NULL) {
-        printf("No nodes found\n");
-    } else if (head->rlink == NULL) {
-        printf("Deleted element is %d\n", head->data);
-        free(head);
-        head = NULL;
-        count--;
-   } else {
-        temp = head;
-        while (temp->rlink != NULL) {
-            temp = temp->rlink;
-        }
-        temp->llink->rlink = NULL;
-        printf("Deleted element is %d\n", temp->data);
-        free(temp);
-        count--;
-    }
-}
-
-void delt_pos() {
-    if (head == NULL) {
-        printf("No nodes found\n");
-    } else {
-        printf("Enter the position to delete: ");
-        scanf("%d", &n);
-        if (n > count) {
-            printf("Deletion not possible\n");
-        } else {
-            int i = 1;
-            temp = head;
-            while (i < n && temp->rlink != NULL) {
-                temp = temp->rlink;
-                i++;
-            }
-            if (temp->llink != NULL) {
-                temp->llink->rlink = temp->rlink;
-            } else {
-                temp->llink->rlink = temp->rlink;
-            }
-            if (temp->rlink != NULL) {
-                temp->rlink->llink = temp->llink;
-            }
-            printf("Deleted element is %d\n", temp->data);
-            free(temp);
-            count--;
-        }
-    }
-}
-
-void search() {
-    int item, i = 1, flag = 0;
-    temp = head;
-    if (temp == NULL) {
-        printf("Empty list\n");
-    } else {
-        printf("Enter item to search: ");
-        scanf("%d", &item);
-        while (temp != NULL) {
-            if (temp->data == item) {
-                printf("Item found at location %d\n", i);
-                flag = 1;
-                break;
-            }
-            temp = temp->rlink;
-            i++;
-        }
-        if (!flag) {
-            printf("Item not found\n");
-        }
-    }
-}
+        int opt;
+        do{
+                printf("\n..................\n");
+                printf("1)PUSH\n");
+                printf("2)POP\n");
+                printf("3)PEEK\n");
+                printf("4)DISPLAY\n");
+                printf("5)QUIET");
+                printf("\n..................\n");
+                printf("choose your option : ");
+                scanf("%d",&opt);
+                switch(opt){
+                        case 1:
+                                push();
+                                break;
+                        case 2:
+                                pop();
+                                break;
+                        case 3:
+                                peek();
+                                break;
+                        case 4:
+                                display();
+                                break;
+                        case 5:
+                                exit(0);
+                    default:
+                        printf("\nInvalid Operation! Try Again... \n");
+                }
+        }while(1);
+        return 0;}   
